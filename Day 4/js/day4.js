39795df2461f79217f525f69023d720586a2ae6d
kmott170 <kdittman@fullsail.edu>
2015-01-13 18:16:58 -0500

    Day4 Arrays Edit
    adding more arrays.

--- Day 4/js/day4.js
+++ Day 4/js/day4.js
@@ -8,3 +8,49 @@ Arrays
 
 //alert("Test Test Test Test");
 
+//Create a basic array
+
+var avengersNames=["Thor", "Iron Man", "Black Widow"];
+
+//Print it out to the console
+
+console.log(avengersNames);
+
+//Print out a specific item
+
+console.log(avengersNames[1]);
+
+//Switch a team member - Black Widow for Hawk-eye
+
+avengersNames[2] = "Hawk-eye";
+
+console.log(avengersNames);
+
+//Add a team member
+
+avengersNames[3] = "Black Widow";
+
+//Print out roster
+
+console.log(avengersNames);
+
+//Use a variable as an index number
+
+var num= 2;
+console.log(avengersNames[num]);
+
+//Length property
+//Dot syntax - fancy for using a period
+
+console.log(avengersNames.length);
+
+//Last index number is the length - 1
+
+//Next open space? Is the length
+
+//Recruit a new team member
+
+avengersNames[avengersNames.length] = "The Hulk";
+console.log(avengersNames);
+
+

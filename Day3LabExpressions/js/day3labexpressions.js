198858701fb2cbd66552eec08abef0e28c5c70df
kmott170 <kdittman@fullsail.edu>
2015-01-13 08:18:19 -0500

    Day3LabExpressions
    this is homework for the day 3 lab expressions assignment.

---
+++ Day3LabExpressions/js/day3labexpressions.js
@@ -0,0 +1,162 @@
+/*
+//Katrina Dittman
+//01/10/2015
+//Lab Number: 00
+*/
+
+//Pizza party
+var numberOfSlices;
+numberOfSlices=80;
+console.log (numberOfSlices);
+
+var numberOfPeopleAtTheParty;
+numberOfPeopleAtTheParty= 8;
+console.log(numberOfPeopleAtTheParty);
+
+var numberOfPizzasOrdered;
+numberOfPizzasOrdered= 10;
+console.log(numberOfPizzasOrdered);
+
+//Solution to find how many pieces of pizza everyone got
+
+//Multiply to find the total amount of slices
+
+var d = 8;
+var e = 10;
+var f = 8 * 10;
+console.log(f);
+
+
+//Multiply to find how many pieces were eaten
+
+var a = 8;
+var b = 8;
+var c = 8 * 8;
+console.log(c);
+
+//Divide to find how many pieces each person got
+
+var g= 64;
+var h= 8;
+var i = 64/8;
+console.log(i);
+
+//How many slices everyone ate
+
+var statement1 = 'Each person ate 8 slices of pizza at the party. ';
+console.log(statement1);
+
+//Slices of pizza that Sparky got
+
+var totalNumberOfSlices;
+totalNumberOfSlices= 80;
+console.log (totalNumberOfSlices);
+
+var totalNumberOfPeopleAtTheParty;
+totalNumberOfPeopleAtTheParty= 8;
+console.log(totalNumberOfPeopleAtTheParty);
+
+var totalNumberOfPizzasOrdered;
+totalNumberOfPizzasOrdered= 10;
+console.log(totalNumberOfPizzasOrdered);
+
+//Solution to find how many pieces Sparky got to eat
+//Subtract total amount of slices from how many eaten
+
+var j=80;
+var k=64;
+var l=80 - 64;
+console.log(l);
+
+var statement2 = 'Sparky got 16 slices of pizza.';
+console.log(statement2);
+
+//Shopping Bill
+
+var week1bill = 60;
+console.log(week1bill);
+
+var week2bill = 100;
+console.log(week2bill);
+
+var week3bill = 80;
+console.log(week3bill);
+
+var week4bill = 50;
+console.log(week4bill);
+
+var week5bill = 50;
+console.log(week5bill);
+
+//Total amount spent
+
+var total = (week1bill + week2bill + week3bill + week4bill + week5bill)
+console.log(total);
+
+//Average weekly grocery spending
+
+var week1bill =60;
+var week2bill =100;
+var week3bill =80;
+var week4bill =50;
+var week5bill =50;
+
+var average = (week1bill + week2bill + week3bill + week4bill + week5bill) /5;
+console.log(average);
+
+var statement3 = 'You have spent a total of $340 on groceries over 5 weeks. That is an average of $68 per week.';
+console.log(statement3);
+
+//Discounts
+
+var op = 150;
+console.log(op);
+
+var dp = 0.2;
+console.log(dp);
+
+var desc ='This item is a beautiful cherry wood computer desk with plenty of shelves and room for storage.';
+console.log(desc);
+
+var stp = 0.5;
+console.log(stp);
+
+//Discounted price
+
+var m = 150;
+var n = 0.2
+var o = m * n
+console.log(o);
+
+var p = 150;
+var q = 30;
+var r = p - q
+console.log(r);
+
+//Price with sales tax
+
+var s = 120;
+var t = 0.5;
+var u = s * t;
+console.log(u);
+
+var v = 120;
+var w = 60;
+var x = v + w;
+console.log(x);
+
+//Price without sales tax
+
+var y = 180;
+var z = 60;
+var a = y - z;
+console.log(a);
+
+var statement4 ='your Computer Desk was originally $150, but after a %20 discount, it is now $120 without tax, and $180 with tax. ';
+console.log(statement4);
+
+
+
+
+
+

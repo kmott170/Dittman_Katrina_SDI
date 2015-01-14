c19e7344465f273c32306ac9c659540e7a16133d
kmott170 <kdittman@fullsail.edu>
2015-01-13 19:57:36 -0500

    User prompting
    prompting users to add information.

--- Day 4/js/day4_prompting.js
+++ Day 4/js/day4_prompting.js
@@ -7,3 +7,26 @@ Prompting
 
 //alert("Testing Test Test Testing Test Test");
 
+//Ask user for input - prompt()
+//Save users response - by creating a variable
+
+var userInput = prompt("Enter your year of birth?");
+console.log(userInput);
+
+//Calculate the area of a triangle
+//are = length * width
+
+//Create 2 variables and prompt the user for length and width
+
+var length = prompt("Let's Calculate the are of a triangle.\nPlease enter length:");
+var width = prompt("Please enter width:");
+
+//Calculate area
+
+var areaRect = length * width;
+console.log("The area of your rectangle with a length of "+length+" and a width of "+width+" is "+areaRect);
+
+
+
+
+

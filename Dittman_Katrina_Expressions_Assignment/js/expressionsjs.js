c724acf7da664eb167f021e65f4ce96c8cd816db
kmott170 <kdittman@fullsail.edu>
2015-01-16 20:42:37 -0500

    Start Of Project
    beginning project.

--- Dittman_Katrina_Expressions_Assignment/js/expressionsjs.js
+++ Dittman_Katrina_Expressions_Assignment/js/expressionsjs.js
@@ -5,20 +5,11 @@ SDI Campus
 Prompting
 */
 
-//alert("Sales Tax and Discount Calculator");
+//alert("The Volume Of A Cylinder & Cone");
 
-//Declaring an array for Original Price + Sales tax
-var arrySt = [];
+//Declaring an array for the volume of a cylinder
+var arryCyl = [];
 
-arrySt[0]= prompt("Please enter original price of item:");
-arrySt[1]= prompt("Please enter sales tax rate in your current state:");
-arrySt[2]= (arrySt[0] * arrySt [1]) + arrySt[0];
-console.log("This is the original price of the item: $" + arrySt[0] + ".\n");
-
-//Declaring an array for Original Price - Discount
-var arryDc = [];
-
-arryDc[0]= prompt("Please enter original price of item:");
-arryDc[1]= prompt("Please enter discount amount as a decimal:");
-arryDc[2]= (arryDc[0] * arryDc[1] - arryDc[0]);
-console.log("This is the original price of the item $" +parseInt( arryDc[0] ) + ".\n");
+//Prompting the user to enter "3.1416" or Pi
+arryCyl[0]= prompt("Please enter the value of Pi: ", "3.1416");
+//Prompting the user to enter a radius in inches

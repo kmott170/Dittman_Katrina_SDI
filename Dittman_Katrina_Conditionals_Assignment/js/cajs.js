4530c0c703b0e2af9b16b1909a6d6a3c0df6f439
kmott170 <kdittman@fullsail.edu>
2015-01-20 10:50:37 -0500

    Conditionals Assignment Start
    entering prompts or grades.

---
+++ Dittman_Katrina_Conditionals_Assignment/js/cajs.js
@@ -0,0 +1,23 @@
+
+ /*
+Katrina Dittman
+SDI Campus
+1/13/2015
+Prompting
+*/
+
+//alert("Testing Test Test Testing Test Test");
+
+//Pass or Fail Calculator - Adding prompts
+//Declaring a variable for grades
+
+ var arryGrade =[];
+
+//Prompting the user to enter grades
+
+ arryGrade[0] = prompt("Please enter grade 1:");
+ arryGrade[1] = prompt("Please enter grade 2:");
+ arryGrade[2] = prompt("Please enter grade 3:");
+ arryGrade[3] = prompt("Please enter grade 4:");
+
+ console.log("The total average of your grades are: ");

35dcf30d8ae402a009f05d4fd6f7d177af405d7f
kmott170 <kdittman@fullsail.edu>
2015-01-20 12:05:58 -0500

    Adding and Editing Calculator and Toal
    adding to calculator code and editing the equation.

--- Dittman_Katrina_Conditionals_Assignment/js/cajs.js
+++ Dittman_Katrina_Conditionals_Assignment/js/cajs.js
@@ -13,11 +13,17 @@ Prompting
 
  var arryGrade =[];
 
-//Prompting the user to enter grades
+//Prompting the user to enter grades 1 - 5
 
  arryGrade[0] = prompt("Please enter grade 1:");
  arryGrade[1] = prompt("Please enter grade 2:");
  arryGrade[2] = prompt("Please enter grade 3:");
  arryGrade[3] = prompt("Please enter grade 4:");
+ arryGrade[4] = prompt("Please enter grade 5:");
 
- console.log("The total average of your grades are: ");
+ //Adding all variables and dividing by the total amount of grades to get avg.
+ arryGrade[5]= arryGrade[4] + arryGrade[3] + arryGrade[2] + arryGrade[1] + arryGrade[0] /5;
+
+ //Printing out total
+ console.log("The total average of your grades are: " + arryGrade[5] );
+

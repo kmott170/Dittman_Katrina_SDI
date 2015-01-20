04fd35cb1d1d44683ac2dc4f9d4f4427740e5c81
kmott170 <kdittman@fullsail.edu>
2015-01-20 18:31:31 -0500

    Basic For Loop Files
    created basic for loop files

---
+++ Day 7/js/day7_forloop.js
@@ -0,0 +1,7 @@
+/*
+ Katrina Dittman
+ 01/20/2015
+ SDI: Day 7 - For Loops
+ */
+
+//alert("Test test test");
--- Dittman_Katrina_Conditionals_Assignment/js/cajs.js
+++ Dittman_Katrina_Conditionals_Assignment/js/cajs.js
@@ -15,15 +15,17 @@ Prompting
 
 //Prompting the user to enter grades 1 - 5
 
- arryGrade[0] = prompt("Please enter grade 1:");
- arryGrade[1] = prompt("Please enter grade 2:");
- arryGrade[2] = prompt("Please enter grade 3:");
- arryGrade[3] = prompt("Please enter grade 4:");
- arryGrade[4] = prompt("Please enter grade 5:");
+ arryGrade[0] = prompt("Please enter grade 1: (Please use whole numbers.)");
+ arryGrade[1] = prompt("Please enter grade 2: (Please use whole numbers.)");
+ arryGrade[2] = prompt("Please enter grade 3: (Please use whole numbers.)");
+ arryGrade[3] = prompt("Please enter grade 4: (Please use whole numbers.)");
+ arryGrade[4] = prompt("Please enter grade 5: (Please use whole numbers.)");
 
  //Adding all variables and dividing by the total amount of grades to get avg.
- arryGrade[5]= arryGrade[4] + arryGrade[3] + arryGrade[2] + arryGrade[1] + arryGrade[0] /5;
+ arryGrade[5]= (parseInt(arryGrade[4]) + parseInt(arryGrade[3]) + parseInt(arryGrade[2]) + parseInt(arryGrade[1]) + parseInt(arryGrade[0])) /5;
+
+
 
  //Printing out total
- console.log("The total average of your grades are: " + arryGrade[5] );
+ console.log("The total average of your grades is: " + arryGrade[5] );
 

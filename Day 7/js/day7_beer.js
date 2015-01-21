b9c9a64cda67f63dc7ff451926817bcd56a2ac70
kmott170 <kdittman@fullsail.edu>
2015-01-20 19:33:21 -0500

    Day 7 Zombies
    zombies and stuff.

---
+++ Day 7/js/day7_beer.js
@@ -0,0 +1,27 @@
+/*
+ Katrina Dittman
+ 01/20/2015
+ SDI: Day 7 Beer
+ */
+
+//alert("Test test test test");
+
+//99 Bottles of Beer On The Wall
+
+for (var i = 99; i > 0; i--) {
+    
+
+
+//Add a check for the last round of beers
+
+if (i ===1) {
+    
+console.log(i+ " bottles of beer on the wall. "+i+" bottles of beer. You take one down and pass it around, "+(i-1)+" bottle of beer on the wall.\nThe End!");
+
+} else{
+    
+console.log(i+ " bottles of beer on the wall. "+i+" bottles of beer. You take one down and pass it around, "+(i-1)+" bottle of beer on the wall.");
+
+}
+
+}

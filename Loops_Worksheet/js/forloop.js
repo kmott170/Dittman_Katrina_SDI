19858824fcd0d6c375a29dd95deca12e0768014c
kmott170 <kdittman@fullsail.edu>
2015-01-22 06:33:16 -0500

    Setting Up Files For Loop Examples
    setting up and adding on to loop example files.

---
+++ Loops_Worksheet/js/forloop.js
@@ -0,0 +1,9 @@
+
+ /*
+Katrina Dittman
+SDI Campus
+1/22/2015
+For Loops
+*/
+
+alert("Testing Test Test Testing Test Test");
---
+++ Loops_Worksheet/js/whileloop.js
@@ -0,0 +1,21 @@
+
+ /*
+Katrina Dittman
+SDI Campus
+1/22/2015
+While Loops
+*/
+
+//alert("Testing Test Test Testing Test Test");
+
+
+/*Here is an example of trying to find out how many horror movies
+ there are in a movie store using the while loop.
+ */
+var howManyHorrorMovies = 0;
+ while (howManyHorrorMovies < 50)
+ {
+ console.log(howManyHorrorMovies + "Horror movies.")
+
+  howManyHorrorMovies++
+ }
